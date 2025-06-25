f3eca1b454bc316601c16bafd50a10f614b66710
alejandro <camacho044@gmail.com>
2025-06-24 22:16:55 -0500

    manejo de excepciones

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { map, Observable } from 'rxjs';
+import { map, Observable, catchError, throwError } from 'rxjs';
 import { RESTCountry } from '../interfaces/rest-countries.interface';
 import { Country } from '../interfaces/country.interface';
 import { CountryMapper } from '../mappers/country.mappers';
@@ -19,7 +19,11 @@ export class CountryService {
 
     return this.http.get<RESTCountry[]>(`${API_URL}/capital/${query}`)
     .pipe(
-      map( (resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp) )
+      map( (resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp) ),
+      catchError(error => {
+        console.log('error fetching', error);
+        return throwError(() => new Error(`No se pudo obtener paises con el valor: ${query}`))
+      })
     )
 
   }
