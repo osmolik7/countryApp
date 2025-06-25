9ac98489b77269adbbc0b48c229ee257008e489c
alejandro <camacho044@gmail.com>
2025-06-25 11:21:16 -0500

    diferentes estados del servicio

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { map, Observable, catchError, throwError } from 'rxjs';
+import { map, Observable, catchError, throwError, delay } from 'rxjs';
 import { RESTCountry } from '../interfaces/rest-countries.interface';
 import { Country } from '../interfaces/country.interface';
 import { CountryMapper } from '../mappers/country.mappers';
@@ -33,6 +33,7 @@ export class CountryService {
     return this.http.get<RESTCountry[]>(`${API_URL}/name/${query}`)
     .pipe(
       map( (resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp) ),
+      delay(2000),
       catchError(error => {
         console.log('error fetching', error);
         return throwError(() => new Error(`No se pudo obtener paises con el valor: ${query}`))
